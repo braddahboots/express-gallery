5177999d7167d41c1373b40adec25be3d2ff38d4
BraddahBoots <bryan@thebrocode.io>
2015-10-28 15:40:41 -1000

    added delete function to details page

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,51 +1,83 @@
 //File path: {{project_directory}}/routes/gallery.js
 var express = require('express');
+var app = express();
 var router = express.Router();
+var db = require('./../models');
+var Gallery = db.gallery;
+var _ = require('lodash');
 
 //GET http://localhost:3000/gallery
 //post a new image to the image gallery array
-router.get('/new', function(req, res) {
-  res.render('index', {
-    imageGallery: [],
-    submitButtonText: 'New Photo'
-  });
+//renders a form that is able to create a new image for the gallery
+router.route('/new')
+  .get(function(req, res) {
+    res.render('newPhoto', {
+    });
 });
 
 //when routed to gallery/:id the user will see a single image
 //should include a link to delete the photo from the gallery
 //should include a link to edit the specific gallery photo
-app.get('/:id', function(req, res) {
-  var photoId = req.params.id;
-  res.sendFile(photoId, function(err) {
-    if(err) {
-      consol.log(err);
-      res.status(err.status).end();
-    } else {
-      consol.log('Sent:', photoId);
-    }
+router.route('/:id')
+  .get(function(req, res) {
+    var photoId = req.params.id;
+    Gallery.findOne({
+      where : {
+        id : photoId
+      }
+    })
+    .then(function(hero_image) {
+      Gallery.findAll({
+        where : {
+          id : {
+            ne : hero_image.id
+          }
+        },
+        limit : 3
+      })
+      .then(function(gallery) {
+        res.render('details', {
+          heroImage: hero_image,
+          // gallery.filter(function(obj) {return obj.id == photoId;})[0],
+          shuffleGallery: _.shuffle(gallery),
+          selectedImage: photoId
+        });
+      });
+    });
   })
+  // .put(function(req, res) {})
   .delete(function(req, res) {
-    res.send('Link to delete image');
-  })
-  .put(function(req, res) {
-    res.send('Link to update image');
+    var photoId = req.params.id;
+    Gallery.destroy({
+      where : {
+        id : photoId
+      }
+    })
+    .then(function(gallery) {
+      res.redirect('/');
+    });
   });
+
+//post a new image to the image gallery array
+router.route('/')
+  .post(function(req, res) {
+    Gallery.create({
+      image: req.body.url,
+      info: req.body.info,
+      link: req.body.link
+    })
+    .then(function(gallery) {
+      res.redirect('/');
+    });
 });
 
 //renders a form to edit a gallery image by it's :id param
-app.get('/:id/edit', function(req, res) {
-  res.redner('editImage', {
-    id: req[i].id,
-    submitButtonText: 'Edit Photo'
-  });
+router.route('/:id/edit')
+  .get(function(req, res) {
+    res.redner('editImage', {
+      id: req[i].id,
+      submitButtonText: 'Edit Photo'
+    });
 });
 
-//updates a single image by it's :id param
-app.put('/:id', function(req, res) {
-  res.send('Updates to photo');
-});
-
-//deletes a single image within gallery by it's :id param
-app.delete('/:id', function(req, res) {
-  res.send('DELETE image');
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var jade = require('jade');
-var listings = require('./listings');
 var db = require('./models');
 var Gallery = db.gallery;
 var _ = require('lodash');
-// var gallery = require('./routes/gallery');
+var bodyParser = require('body-parser');
+var galleryRouter = require('./routes/gallery');
+var methodOverride = require('method-override');
 
 //tell express which template engine we are using by NPM module name
 app.set('view engine', 'jade');
@@ -16,95 +16,30 @@ app.set('views', './views/');
 //for any static request to express use this path and all files within
 app.use(express.static('./public'));
 
+//parse application
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+//start router
+app.use('/gallery', galleryRouter);
+
 //root directory will render the list gallery photos located within index
 app.get('/', function(req, res) {
   Gallery.findAll()
     .then(function(gallery){
       res.render('index', {
-        imageGallery: gallery,
+        imageGallery: gallery
       });
     });
 });
-
-//renders a form that is able to create a new image for the gallery
-app.get('/gallery/new', function(req, res) {
-  res.render('newPhoto', {
-    submitButtonText: 'New Photo'
-  });
-});
-
-//when routed to gallery/:id the user will see a single image
-//should include a link to delete the photo from the gallery
-//should include a link to edit the specific gallery photo
-app.get('/gallery/:id', function(req, res) {
-  var photoId = req.params.id;
-  Gallery.findOne({
-    where : {
-      id : photoId
-    }
-  })
-  .then(function(hero_image){
-    Gallery.findAll({
-      where : {
-        id : {
-          ne : hero_image.id
-        }
-      },
-      limit : 3
-    })
-    .then(function(gallery){
-      res.render('details', {
-        heroImage: hero_image,
-        // gallery.filter(function(obj) {return obj.id == photoId;})[0],
-        shuffleGallery: _.shuffle(gallery),
-        selectedImage: photoId
-      });
-    });
-  });
-});
-
-//post a new image to the image gallery array
-app.post('/gallery', function(req, res) {
-  Gallery.create({
-    image: req.body.url,
-    info: req.body.info,
-    link: req.body.link
-  })
-  .then(function(gallery) {
-    res.render('index', {
-      imageGallery: gallery
-    });
-  });
-  res.send('New instance of a image to add to gallery');
-});
-
-//updates a single image by it's :id param
-app.put('/gallery/:id', function(req, res) {
-  res.send('Updates to photo');
-});
-
-//deletes a single image within gallery by it's :id param
-app.delete('/gallery/:id', function(req, res) {
-  res.send('DELETE image');
-});
-
-
-//renders a form to edit a gallery image by it's :id param
-app.get('/gallery/:id/edit', function(req, res) {
-  res.redner('editImage', {
-    id: req[i].id,
-    submitButtonText: 'Edit Photo'
-  });
-});
-
 
 //create server and listen to address 3000
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Photo gallery server listening at http://%s:%s', host, port);
-});
-
-
-
+var server = app.listen(3000, function() {});
